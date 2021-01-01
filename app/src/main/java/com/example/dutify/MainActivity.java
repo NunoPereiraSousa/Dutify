dd5748e06ecb3a0a7490ae861931e30b2777f8ec
Nuno Sousa <9180579@esmad.ipp.pt>
2021-01-01 23:25:51 +0000

    +3 Layouts done

--- app/src/main/java/com/example/dutify/MainActivity.java
+++ app/src/main/java/com/example/dutify/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.dutify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void welcome(View view) {
+        Intent intent = new Intent(this, Welcome.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
