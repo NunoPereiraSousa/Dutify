0f3de7d642865c3b4ff04ed43d45015dc3e2e4c8
Tiago De Pina <tiagopina20014@gmail..com>
2021-01-07 00:52:30 +0000

    DashboardBoard and MotivationalSmsSection added

---
+++ app/src/main/java/com/example/dutify/MotivationalSmsSection.java
@@ -0,0 +1,14 @@
+package com.example.dutify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotivationalSmsSection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motivational_sms_section);
+    }
+}
