556617fa759c30e36282231d7938d7911dafda3e
Tiago De Pina <tiagopina20014@gmail..com>
2021-01-08 11:03:08 +0000

    Calendar added and error adjustments

--- app/src/main/java/com/example/dutify/MainActivity.java
+++ app/src/main/java/com/example/dutify/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, Calendar.class);
         startActivity(intent);
     }
 
