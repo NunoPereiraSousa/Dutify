a64c91575849dedd58332ea4b436cdc1203bc48e
Tiago De Pina <tiagopina20014@gmail..com>
2021-01-09 22:08:10 +0000

    CalendarLayout almost done, todo card added and calendar form added

--- app/src/main/java/com/example/dutify/Calendar.java
+++ app/src/main/java/com/example/dutify/Calendar.java
@@ -2,13 +2,32 @@ package com.example.dutify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Calendar extends AppCompatActivity {
+
+    TextView addPersonalTaskBtnLabel;
+    Dialog myDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        definePopUpForm();
+        addPersonalTaskBtnLabel= (TextView) findViewById(R.id.addPersonalTaskBtnLabel);
+        addPersonalTaskBtnLabel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.show();
+            }
+        });
+    }
+    public void definePopUpForm(){
+        myDialog= new Dialog(this);
+        myDialog.setContentView(R.layout.fragment_personal_task_adding_form);
     }
 }
